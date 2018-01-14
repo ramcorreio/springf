ce520a77214f54562433362af79d0ff37ba0e61b
Rodrigo Macedo <rodrigo.macedo@datagrupo.com.br>
2018-01-13 23:26:20 -0200

    inclusão de novos testes

--- src/test/java/club/colab4p/springf/repo/CustomerRepositoryTest.java
+++ src/test/java/club/colab4p/springf/repo/CustomerRepositoryTest.java
@@ -1,12 +1,15 @@
 package club.colab4p.springf.repo;
 
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,20 +38,42 @@ public class CustomerRepositoryTest {
 		List<Customer> rs1 = repository.findByLastName("Macedo");
 		assertThat(rs1, is(empty()));
 		
+		long totalCustomersBefore = repository.count();
 		Customer rodrigo = new Customer();
 		rodrigo.setFirstName("Rodrigo");
 		rodrigo.setLastName("Macedo");
-		repository.saveAndFlush(rodrigo);
+		repository.save(rodrigo);
 		
 		Customer luciana = new Customer();
 		luciana.setFirstName("Luciana");
 		luciana.setLastName("Neri");
-		repository.saveAndFlush(luciana);
+		repository.save(luciana);
 		
 		List<Customer> rs2 = repository.findByLastName("Macedo");
 		assertThat(rs2, hasSize(1));
 		
 		List<Customer> rs3 = repository.findAll();
-		assertThat(rs3, hasSize(2));
+		assertThat(rs3, hasSize(2 + Long.valueOf(totalCustomersBefore).intValue()));
+	}
+	
+	@Test
+	public void count() {
+		
+		Random r = new Random();
+		long totalCustomersBefore = repository.count();
+		long totalCustomers = r.nextInt(1000);
+		
+		List<Customer> customers = new ArrayList<>();
+		for (int i = 0; i < totalCustomers; i++) {
+			
+			int index = i + 1;
+			Customer c = new Customer();
+			c.setFirstName("First " + index);
+			c.setLastName("Last " + index);
+			customers.add(c);
+		}
+		
+		repository.saveAll(customers);
+		assertThat(totalCustomers, is(equalTo(repository.count() + Long.valueOf(totalCustomersBefore).intValue())));
 	}
 }
